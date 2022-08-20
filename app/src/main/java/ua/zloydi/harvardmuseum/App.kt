62f4041842b363410dfef839b6563482c394dbe8
zloydi <dobrovolskyivan2002@gmail.com>
2022-08-20 11:34:05 +0200

    Added base classes

---
+++ app/src/main/java/ua/zloydi/harvardmuseum/App.kt
@@ -0,0 +1,5 @@
+package ua.zloydi.harvardmuseum
+
+import android.app.Application
+
+class App : Application() {}
---
+++ app/src/main/java/ua/zloydi/harvardmuseum/ui/Activity.kt
@@ -0,0 +1,12 @@
+package ua.zloydi.harvardmuseum.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ua.zloydi.harvardmuseum.R
+
+class Activity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity)
+	}
+}
