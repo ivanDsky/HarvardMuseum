b344b529c059adfcc09cc71d50fc8a55254563ec
zloydi <dobrovolskyivan2002@gmail.com>
2022-08-21 16:06:47 +0200

    Added retrofit module with dagger provides

---
+++ core/retrofit/src/main/java/ua/zloydi/retrofit/Dagger.kt
@@ -0,0 +1,31 @@
+package ua.zloydi.retrofit
+
+import dagger.Module
+import dagger.Provides
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+class RetrofitModule {
+	@Provides
+	@Singleton
+	fun getClient(): OkHttpClient =
+		OkHttpClient.Builder()
+			.callTimeout(5, TimeUnit.SECONDS)
+			.readTimeout(5, TimeUnit.SECONDS)
+			.writeTimeout(5, TimeUnit.SECONDS)
+			.build()
+	
+	@Provides
+	@Singleton
+	fun getRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
+		.addConverterFactory(GsonConverterFactory.create())
+		.addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+		.baseUrl(BuildConfig.BASE_URL)
+		.client(client)
+		.build()
+}
