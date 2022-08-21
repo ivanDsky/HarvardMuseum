c4c1b639e265781675ce0323a192567e263fc800
zloydi <dobrovolskyivan2002@gmail.com>
2022-08-21 16:08:10 +0200

    Added test retrofit settings

---
+++ test/retrofit/src/main/java/ua/zloydi/test/retrofit/Dagger.kt
@@ -0,0 +1,24 @@
+package ua.zloydi.test.retrofit
+
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockWebServer
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitTest {
+	val client = OkHttpClient.Builder()
+			.callTimeout(5, TimeUnit.SECONDS)
+			.readTimeout(5, TimeUnit.SECONDS)
+			.writeTimeout(5, TimeUnit.SECONDS)
+			.build()
+	
+	fun getRetrofit(url: HttpUrl) = Retrofit.Builder()
+		.addConverterFactory(GsonConverterFactory.create())
+		.addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+		.baseUrl(url)
+		.client(client)
+		.build()
+}
